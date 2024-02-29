e838121ab22d963c0637287be1b3270427c413bb
kaifk2 <118038413+kaifk2@users.noreply.github.com>
2024-02-29 10:22:56 +0530

    tu\\

--- src/main/java/com/tunhub/controller/PaymentController.java
+++ src/main/java/com/tunhub/controller/PaymentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
-import com.razorpay.Utils;
 import com.tunhub.entities.Users;
 import com.tunhub.services.UsersService;
 
@@ -26,25 +25,25 @@ public class PaymentController {
 	public String pay() {
 		return "pay";
 	}
-	
+
 	@SuppressWarnings("finally")
 	@PostMapping("/createOrder")
 	@ResponseBody
 	public String createOrder(HttpSession session) {
 
-		int  amount  = 5000;
-		Order order=null;
+		int amount = 5000;
+		Order order = null;
 		try {
-			RazorpayClient razorpay=new RazorpayClient("rzp_test_d4GvaeJbe7Szp2", "lg53AxE71A6l3A3PUAnJYdyh");
+			RazorpayClient razorpay = new RazorpayClient("rzp_test_LZn9kTq0XH2NI2", "PQAF8w9oqZrWizkYX0G3RABH");
 
 			JSONObject orderRequest = new JSONObject();
-			orderRequest.put("amount", amount*100); // amount in the smallest currency unit
+			orderRequest.put("amount", amount * 100); // amount in the smallest currency unit
 			orderRequest.put("currency", "INR");
 			orderRequest.put("receipt", "order_rcptid_11");
 
 			order = razorpay.orders.create(orderRequest);
 
-			String mail =  (String) session.getAttribute("email");
+			String mail = (String) session.getAttribute("email");
 
 			Users u = service.getUser(mail);
 			u.setPremium(true);
@@ -52,28 +51,10 @@ public class PaymentController {
 
 		} catch (RazorpayException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			return order.toString();
 		}
 	}
-	
-	@PostMapping("/verify")
-	@ResponseBody
-	public boolean verifyPayment(@RequestParam  String orderId, @RequestParam String paymentId, @RequestParam String signature) {
-	    try {
-	        // Initialize Razorpay client with your API key and secret
-	        RazorpayClient razorpayClient = new RazorpayClient("rzp_test_pvGIc9JvXWZTVS", "UlnLCUb8lRxvBKRyIYRYWrEO");
-	        // Create a signature verification data string
-	        String verificationData = orderId + "|" + paymentId;
 
-	        // Use Razorpay's utility function to verify the signature
-	        boolean isValidSignature = Utils.verifySignature(verificationData, signature, "UlnLCUb8lRxvBKRyIYRYWrEO");
 
-	        return isValidSignature;
-	    } catch (RazorpayException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
-	}
 }
